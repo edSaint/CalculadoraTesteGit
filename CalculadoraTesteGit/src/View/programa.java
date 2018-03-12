a4022ebe3230bb2a925ab6415ddab5b7d0e2c291
Edson Carvalho <ecarvalhost@gmail.com>
2018-03-12 17:11:07 -0300

    1
    Criando metodo de entrada Scanner

--- CalculadoraTesteGit/src/View/programa.java
+++ CalculadoraTesteGit/src/View/programa.java
@@ -1,10 +1,16 @@
 package View;
 
+import java.util.Scanner;
+
 public class programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner teclado = new Scanner(System.in);
+		
+		
+		
+		
 	}
 
 }
